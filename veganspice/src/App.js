69d29cccc5fcc1f9694e1310681c09501148ec5c
Saumya Shekhar <www.sidharth.56@gmail.com>
2023-10-01 11:13:32 +0530

    Added logo, menu, login tamplate

--- veganspice/src/App.js
+++ veganspice/src/App.js
@@ -1,13 +1,17 @@
-import { Route, Router, Routes } from 'react-router-dom';
-import './App.css';
-import Home from './screens/Home';
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import "./App.css";
+import Home from "./screens/Home";
+import Login from "./screens/Login";
+import Menu from "./screens/Menu";
 
 function App() {
   return (
     <div className="App h-full">
       <Router>
         <Routes>
-          <Route exact path='/' element={<Home/>}></Route>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/login" element={<Login />} />
+          <Route exact path="/menu" element={<Menu />} />
         </Routes>
       </Router>
     </div>
