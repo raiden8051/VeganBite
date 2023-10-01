617e87438176761f2c16495dce85cba837095862
Saumya Shekhar <www.sidharth.56@gmail.com>
2023-10-01 16:51:22 +0530

    Added static food page

---
+++ src/components/FoodSection.js
@@ -0,0 +1,10 @@
+import React from "react";
+import FoodCard from "./elements/FoodCard";
+
+export default function body() {
+  return (
+    <div className="mt-5 ml-10">
+      <FoodCard />
+    </div>
+  );
+}
